9ff08ecff37f871b98dfc18c0ad492688b20e53b
Vladyslav Miliev <84059723+VladyslavMilievNotAbot@users.noreply.github.com>
2023-11-01 23:51:05 +0200

    Updated neutral.js
    I've added the calculation of intervals with aggressive estimates

--- neutral.js
+++ neutral.js
@@ -1,50 +1,25 @@
 import { intervalEstimates } from "./intervalEstimateTrapLt.js";
 import { trapLtMatrix } from "./trapLtTable.js";
 
-function calculateNeutralPosition(intervalEstimates, trapLtMatrix) {
+function calculateNeutralPosition() {
   const neuContainer = document.getElementById("neuContainer");
-  const probabilities = [];
 
-  let neutralHTML = "<strong>The neutral position:</strong><br>";
-
-  const averageTrapezoidalTerms = calculateAverageTrapezoidalTerms(trapLtMatrix);
-  neutralHTML += "<strong>Average trapezoidal terms</strong><br>";
-  for (let i = 0; i < averageTrapezoidalTerms.length; i++) {
-    neutralHTML += `(${averageTrapezoidalTerms[i].join(", ")})<br>`;
-  }
-
-  for (let rowIndex = 0; rowIndex < intervalEstimates.length; rowIndex++) {
-    const row = intervalEstimates[rowIndex];
-
-    let I_L = null;
-    let I_R = null;
-
-    for (let j = 0; j < row.length; j++) {
-      const interval = {
-        left: parseFloat(row[j][0]),
-        right: parseFloat(row[j][1]),
-      };
-
-      if (I_L === null || interval.left < I_L) {
-        I_L = interval.left;
-      }
-
-      if (I_R === null || interval.right > I_R) {
-        I_R = interval.right;
-      }
-    }
+  const probabilities = intervalEstimates.map((row, rowIndex) => {
+    const [I_L, I_R] = row.reduce(
+      ([minLeft, maxRight], [left, right]) => [
+        Math.min(minLeft, parseFloat(left)),
+        Math.max(maxRight, parseFloat(right)),
+      ],
+      [null, null]
+    );
 
     const I_neutral = [I_L, I_R];
-    const probability = Math.max(
-      1 - Math.max((1 - I_L) / (I_R - I_L + 1), 0),
-      0
-    ).toFixed(2);
+    const probability = (1 - Math.max((1 - I_L) / (I_R - I_L + 1), 0)).toFixed(
+      2
+    );
 
-    probabilities.push({ index: rowIndex + 1, probability });
-
-    neutralHTML += `I(x${rowIndex + 1})=[${I_L.toFixed(2)}, ${I_R.toFixed(2)}]<br>`;
-    neutralHTML += `p(I(x${rowIndex + 1}))=max(1-max((1-${I_L.toFixed(2)})/(${I_R.toFixed(2)}-${I_L.toFixed(2)}+1),0)) = ${probability}<br>`;
-  }
+    return { index: rowIndex + 1, probability, I_L, I_R };
+  });
 
   probabilities.sort((a, b) => b.probability - a.probability);
 
@@ -59,44 +34,88 @@ function calculateNeutralPosition(intervalEstimates, trapLtMatrix) {
       }
     })
     .join("");
-  neuContainer.innerHTML =
-    neutralHTML + "<strong>Ranking:</strong><br>" + rankingHTML;
+
+  const neutralHTML = probabilities
+    .map(
+      (item) =>
+        `I(x${item.index})=[${item.I_L.toFixed(2)}, ${item.I_R.toFixed(
+          2
+        )}]<br>` +
+        `p(I(x${item.index}))=max(1-max((1-${item.I_L.toFixed(
+          2
+        )})/(${item.I_R.toFixed(2)}-${item.I_L.toFixed(2)}+1),0)) = ${
+          item.probability
+        }<br>`
+    )
+    .join("");
+
+  neuContainer.innerHTML = `<strong>The neutral position:</strong><br>${neutralHTML}<strong>Ranking:</strong><br>${rankingHTML}`;
+}
+function calculateAggressiveEstimates(intervalEstimates) {
+  const aggrContainer = document.getElementById("aggrContainer");
+  const probabilities = [];
+
+  let aggrHTML = "<strong>Intervals with aggressive estimates:</strong><br>";
+
+  intervalEstimates.forEach((row, rowIndex) => {
+    const Imin = [0, 0];
+    const Imax = [0, 0];
+
+    row.forEach(([leftStr, rightStr]) => {
+      const left = parseFloat(leftStr);
+      const right = parseFloat(rightStr);
+
+      Imin[0] += left;
+      Imin[1] += right;
+      Imax[0] += left;
+      Imax[1] += right;
+    });
+
+    const I_L = Imin[0] / 2;
+    const I_R = Imax[1] / 2;
+
+    const probability = calculateProbability(I_L, I_R);
+
+    probabilities.push({ index: rowIndex + 1, probability });
+
+    aggrHTML += `Iag(x${rowIndex + 1})=[(${Imin[0].toFixed(
+      2
+    )}+${Imax[0].toFixed(2)})/2, (${Imin[1].toFixed(2)}+${Imax[1].toFixed(
+      2
+    )})/2]=[${I_L.toFixed(2)}, ${I_R.toFixed(2)}]<br>`;
+    aggrHTML += `p(Iag(x${rowIndex + 1}))=max(1-max((1-${I_L.toFixed(
+      2
+    )})/(${I_R.toFixed(2)}-${I_L.toFixed(2)}+1),0)) = ${probability.toFixed(
+      2
+    )}<br>`;
+  });
+
+  probabilities.sort((a, b) => b.probability - a.probability);
+
+  const rankingHTML = probabilities
+    .map((item, index, arr) => {
+      if (index === 0) {
+        return `x${item.index}`;
+      } else if (item.probability === arr[index - 1].probability) {
+        return ` = x${item.index}`;
+      } else {
+        return ` > x${item.index}`;
+      }
+    })
+    .join("");
+
+  aggrContainer.innerHTML = `${aggrHTML}<strong>Ranking:</strong><br>${rankingHTML}`;
 }
 
-function calculateAverageTrapezoidalTerms(trapLtMatrix) {
-  const avgTrapezoidalTerms = [];
-
-  for (let colIndex = 0; colIndex < trapLtMatrix[0].length; colIndex++) {
-    let sum_a = 0;
-    let sum_b = 0;
-    let sum_c = 0;
-    let sum_d = 0;
-
-    for (let rowIndex = 0; rowIndex < trapLtMatrix.length; rowIndex++) {
-      const trapLt = trapLtMatrix[rowIndex][colIndex];
-      sum_a += trapLt[0];
-      sum_b += trapLt[1];
-      sum_c += trapLt[2];
-      sum_d += trapLt[3];
-    }
-
-    const avg_a = (sum_a / trapLtMatrix.length).toFixed(2);
-    const avg_b = (sum_b / trapLtMatrix.length).toFixed(2);
-    const avg_c = (sum_c / trapLtMatrix.length).toFixed(2);
-    const avg_d = (sum_d / trapLtMatrix.length).toFixed(2);
-
-    avgTrapezoidalTerms.push([avg_a, avg_b, avg_c, avg_d]);
-  }
-
-  return avgTrapezoidalTerms;
+function calculateProbability(I_L, I_R) {
+  return Math.max(1 - Math.max((1 - I_L) / (I_R - I_L + 1), 0), 0);
 }
 
-
-// Call the function when the DOM content is loaded
 document.addEventListener("DOMContentLoaded", function () {
   const calcNeutralBtn = document.getElementById("calcNeutral");
 
   calcNeutralBtn.addEventListener("click", function () {
-    calculateNeutralPosition(intervalEstimates, trapLtMatrix);
+    calculateNeutralPosition();
+    calculateAggressiveEstimates(intervalEstimates);
   });
 });
